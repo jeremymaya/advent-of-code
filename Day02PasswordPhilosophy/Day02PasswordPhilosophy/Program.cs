4dc0db08c855950424a724f46b7c20bb182229c9
Kyungrae Kim <kyungraekim@outlook.com>
2020-12-06 22:48:18 -0800

    2020 day 02 complete

--- Day02PasswordPhilosophy/Day02PasswordPhilosophy/Program.cs
+++ Day02PasswordPhilosophy/Day02PasswordPhilosophy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Day02PasswordPhilosophy
 {
@@ -6,7 +7,49 @@ namespace Day02PasswordPhilosophy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = "../../../passwords.txt";
+
+            int valid = PasswordPhilosophy(path);
+
+            Console.WriteLine(valid);
+        }
+
+        static int PasswordPhilosophy(string path)
+        {
+            int valid = 0;
+
+            try
+            {
+                using var sr = new StreamReader(path);
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(' ');
+                    string[] range = line[0].Split('-');
+
+                    int appear = CountLetter(line[1][0], line[2]);
+
+                    if (appear >= Int32.Parse(range[0]) && appear <= Int32.Parse(range[1]))
+                        valid++;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return valid;
+        }
+
+        static int CountLetter(char letter, string password)
+        {
+            int appear = 0;
+
+            for (int i = 0; i < password.Length; i++)
+                if (letter == password[i])
+                    appear++;
+
+            return appear;
         }
     }
 }
