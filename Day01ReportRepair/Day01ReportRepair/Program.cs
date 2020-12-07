158174ed7a0445a5217167c634dc1b385bcaffde
Kyungrae Kim <kyungraekim@outlook.com>
2020-12-06 21:33:42 -0800

    scaffolding 2020 day 01

---
+++ Day01ReportRepair/Day01ReportRepair/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day01ReportRepair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
