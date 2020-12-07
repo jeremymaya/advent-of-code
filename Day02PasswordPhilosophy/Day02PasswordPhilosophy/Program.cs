0b472813176aadb19c223d54c12344f50704e046
Kyungrae Kim <kyungraekim@outlook.com>
2020-12-06 22:15:02 -0800

    scaffolding for 2020 day 02

---
+++ Day02PasswordPhilosophy/Day02PasswordPhilosophy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day02PasswordPhilosophy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
