fd6551dcadc8ab3444fbc916caceafa7b0e7f692
Kyungrae Kim <kyungraekim@outlook.com>
2020-12-10 20:06:53 -0800

    scaffolding day 03

---
+++ Day03TobogganTrajectory/Day03TobogganTrajectory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day03TobogganTrajectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
